69d1888fb402bfcca03ed85bd9c8f379dfddb9bb
aaron <aaronelbarbaroii@gmail.com>
2025-11-05 12:42:41 +0100

    Add message for Obesity and extreme Obesity

--- IMC-IOS/MainViewController.swift
+++ IMC-IOS/MainViewController.swift
@@ -38,20 +38,23 @@ class MainViewController: UIViewController {
     @IBAction func weightStepper(_ sender: UIStepper) {
         weight = Int(sender.value)
         weightLabel.text = "\(weight) kg"
-        calculateButton()
+        // calculateButton()
     }
     
     @IBAction func heightSlider(_ sender: UISlider) {
         height = Int(sender.value)
         heightLabel.text = "\(height) cm"
-        calculateButton()
+        // calculateButton()
     }
     
-    func calculateButton() {
+    @IBAction func calculateButton() {
         let result = ("\(Double(weight)/pow(Double(height)/100.0, 2))")
         resultLabel.text = String(format: "%.2f", Double(result)!)
         estado.text = estadoIMC(imc: Double(result)!)
     }
+    
+    
+
     
     func estadoIMC(imc: Double) -> String {
         if imc < 18.5 {
@@ -69,13 +72,39 @@ class MainViewController: UIViewController {
         } else if imc < 34.9 {
             estado.textColor = .bmiColorObesity
             resultLabel.textColor = .bmiColorObesity
+            mostrarAlerta(estado: "obesidad")
             return "Obesidad"
         }
         else {
             estado.textColor = .bmiColorExtremeObesity
             resultLabel.textColor = .bmiColorExtremeObesity
+            mostrarAlerta(estado: "morbida")
             return "Obesidad mórbida"
         }
     }
+    
+    // Dentro de una acción de un botón o método, por ejemplo:
+    func mostrarAlerta(estado: String) {
+        var messageText: String = ""
+        
+        if(estado == "obesidad"){
+            messageText = "Tiene Obesidad. Debería consultar a su médico"
+        }
+        else {
+            messageText = "Tiene Obesidad mórbida. Debería consultar a su médico"
+        }
+        
+        
+        let alert = UIAlertController(title: "Atención",
+                                      message: "\(messageText)",
+                                      preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            // Código a ejecutar al presionar OK
+        }))
+
+        self.present(alert, animated: true, completion: nil)
+    }
+
     
 }
